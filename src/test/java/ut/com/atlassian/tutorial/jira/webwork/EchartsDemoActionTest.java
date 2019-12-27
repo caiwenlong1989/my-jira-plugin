a3fe0e5a8a86062c7e155c5d8b6e55c40e268410
Caiwenlong <594665291@qq.com>
2019-12-27 17:46:26 +0800

    添加Webwork

---
+++ src/test/java/ut/com/atlassian/tutorial/jira/webwork/EchartsDemoActionTest.java
@@ -0,0 +1,34 @@
+package ut.com.atlassian.tutorial.jira.webwork;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import com.atlassian.tutorial.jira.webwork.EchartsDemoAction;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @since 3.5
+ */
+public class EchartsDemoActionTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test(expected=Exception.class)
+    public void testSomething() throws Exception {
+
+        //EchartsDemoAction testClass = new EchartsDemoAction();
+
+        throw new Exception("EchartsDemoAction has no tests!");
+
+    }
+
+}
